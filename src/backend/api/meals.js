d9d37e2db1539241ec7f46e650bc0d6c0de71248
ayaroshenkocom <mailfortools@gmail.com>
2023-11-29 18:28:09 +0100

    Meal-sharing homework

--- src/backend/api/meals.js
+++ src/backend/api/meals.js
@@ -2,7 +2,10 @@ const express = require("express");
 const router = express.Router();
 const knex = require("../database");
 
-router.get("/", async (request, response) => {
+let date = new Date();
+const formattedCurrentDate = date.toISOString().slice(0, 19).replace('T', ' ');
+
+router.get("/meals", async (request, response) => {
   try {
     // knex syntax for selecting things. Look up the documentation for knex for further info
     const titles = await knex("meals").select("title");
@@ -11,5 +14,66 @@ router.get("/", async (request, response) => {
     throw error;
   }
 });
+
+
+router.get("/future-meals", async (request, response) => {
+  try {
+    // knex syntax for selecting things. Look up the documentation for knex for further info
+    const futureMeals = await knex("meals").select(["id", "title", "description", "price"]).where("meal_when", ">", formattedCurrentDate);
+    response.json(futureMeals);
+  } catch (error) {
+    throw error;
+  }
+});
+
+router.get("/past-meals", async (request, response) => {
+  try {
+    // knex syntax for selecting things. Look up the documentation for knex for further info
+    const pastMeals = await knex("meals").select(["id", "title", "description", "price"]).where("meal_when", "<", formattedCurrentDate);
+    response.json(pastMeals);
+  } catch (error) {
+    throw error;
+  }
+});
+
+
+router.get("/all-meals", async (request, response) => {
+  try {
+    // knex syntax for selecting things. Look up the documentation for knex for further info
+    const allMeals = await knex("meals").select(["id", "title", "description", "price"]).orderBy('id', 'desc');
+    response.json(allMeals);
+  } catch (error) {
+    throw error;
+  }
+});
+
+router.get("/first-meal", async (request, response) => {
+  try {
+    const firstMeal = await knex("meals").select(["id", "title", "description", "price"]).orderBy('id', 'asc').first();
+
+    if (firstMeal) {
+      response.json(firstMeal);
+    } else {
+      response.status(404).json({ message: "No meals available." });
+    }
+  } catch (error) {
+    throw error;
+  }
+});
+
+router.get("/last-meal", async (request, response) => {
+  try {
+    const lastMeal = await knex("meals").select(["id", "title", "description", "price"]).orderBy('id', 'desc').first();
+
+    if (lastMeal) {
+      response.json(lastMeal);
+    } else {
+      response.status(404).json({ message: "No meals available." });
+    }
+  } catch (error) {
+    throw error;
+  }
+});
+
 
 module.exports = router;
