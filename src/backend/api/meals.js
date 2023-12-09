e6364f21b25fae3676f3ae0a9bf53215a720174d
ayaroshenkocom <mailfortools@gmail.com>
2023-12-09 21:01:39 +0100

    NodeJS homework week2

--- src/backend/api/meals.js
+++ src/backend/api/meals.js
@@ -7,16 +7,15 @@ const formattedCurrentDate = date.toISOString().slice(0, 19).replace('T', ' ');
 
 router.get("/meals", async (request, response) => {
   try {
-    // knex syntax for selecting things. Look up the documentation for knex for further info
-    const titles = await knex("meals").select("title");
-    response.json(titles);
+    const meals = await knex("meals").select("*");
+    response.json(meals);
   } catch (error) { response.status(500).json({ error: "An unexpected error occurred while processing your request." }); }
 });
 
 
 router.get("/future-meals", async (request, response) => {
   try {
-    // knex syntax for selecting things. Look up the documentation for knex for further info
+
     const futureMeals = await knex("meals").select(["id", "title", "description", "price"]).where("meal_when", ">", formattedCurrentDate);
     response.json(futureMeals);
   } catch (error) {
@@ -29,7 +28,7 @@ router.get("/future-meals", async (request, response) => {
 
 router.get("/past-meals", async (request, response) => {
   try {
-    // knex syntax for selecting things. Look up the documentation for knex for further info
+
     const pastMeals = await knex("meals").select(["id", "title", "description", "price"]).where("meal_when", "<", formattedCurrentDate);
     response.json(pastMeals);
   } catch (error) {
@@ -40,7 +39,7 @@ router.get("/past-meals", async (request, response) => {
 
 router.get("/all-meals", async (request, response) => {
   try {
-    // knex syntax for selecting things. Look up the documentation for knex for further info
+
     const allMeals = await knex("meals").select(["id", "title", "description", "price"]).orderBy('id', 'desc');
     response.json(allMeals);
   } catch (error) {
@@ -58,7 +57,7 @@ router.get("/first-meal", async (request, response) => {
       response.status(404).json({ message: "No meals available." });
     }
   } catch (error) {
-    throw error;
+    response.status(500).json({ error: "An unexpected error occurred while processing your request." });
   }
 });
 
@@ -74,6 +73,52 @@ router.get("/last-meal", async (request, response) => {
   } catch (error) {
     response.status(500).json({ error: "An unexpected error occurred while processing your request." });
   }
+});
+
+
+router.post("/meals", async (request, response) => {
+  const body = request.body;
+  body.created_date = new Date();
+
+  try {
+    await knex("meals").insert(body);
+    response.status(201).json("New meal entry has been created!");
+  } catch (error) { response.status(500).json({ error: "An unexpected error occurred while processing your request:", message: error.message }); }
+});
+
+
+router.get("/meals/:id", async (request, response) => {
+  const id = request.params.id;
+  try {
+    const meal = await knex("meals").select("*").where("id", "=", id);
+
+    if (meal.length) {
+      response.json(meal);
+    } else {
+      response.status(404).json({ message: "No meals available." });
+    }
+  } catch (error) {
+    response.status(404).json({ error: "An unexpected error occurred while processing your request." });
+  }
+});
+
+router.put("/meals/:id", async (request, response) => {
+  const id = request.params.id;
+  const body = request.body;
+
+  try {
+    await knex("meals").update(body).where("id", "=", id);
+    response.status(200).json("Meal entry has been updated!");
+  } catch (error) { response.status(404).json({ error: "An unexpected error occurred while processing your request:", message: error.message }); }
+});
+
+router.delete("/meals/:id", async (request, response) => {
+  const id = request.params.id;
+
+  try {
+    await knex("meals").delete().where("id", "=", id);
+    response.status(204).json("Meal entry has been deleted!");
+  } catch (error) { response.status(404).json({ error: "An unexpected error occurred while processing your request:", message: error.message }); }
 });
 
 
